ca2b6f31ba6450046c66d4ee280909b10f5fe455
user.email <vvshumilov@mail.ru>
2026-04-26 23:24:17 +0200

    версия кода 29, версия приложения 13.1 - обновлен дизайн приложения

---
+++ app/src/main/java/com/vshum/turbogum/Constants.kt
@@ -0,0 +1,10 @@
+package com.vshum.turbogum
+
+class Constants {
+    companion object {
+        const val PASSWORD = "123"
+        const val JSON_LINERS = "dataLiners.json"
+        const val URL = "https://raw.githubusercontent.com/VyacheslavShumilov/Base/main/"
+        const val POLICY = "https://vyachess14607.wixsite.com/railcarinfo"
+    }
+}
